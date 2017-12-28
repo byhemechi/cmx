da1a0dc267ef92bcaac1d4cc7c4709fd4b4ab49b
George Fischer <mrpyber@outlook.com>
2017-12-28 15:52:35 +1100

    make the server begin to work

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
 const config = require("./config/config.def");
+const fs = require('fs');
+const path = require('path');
 
 // Config
 Object.assign(config, require("./config/config"));
 app.set("view engine", "pug");
 
+// Routing
+app.use("/assets", express.static(path.join(__dirname, "build")));
+app.use("/assets", express.static(path.join(__dirname, "client")));
+
 // Handle requests
-app.get("/", function(req, res) {
-  res.render("page", {
-    title: "Test Page",
-    content: "this is a test page. Yadda </strong>Yadda</strong> Yadda"
+app.use("/", function(req, res) {
+  fs.readFile(`${path.join(__dirname, "/pages/", req.url.replace(/^\/$/, "home").replace(/\/$/, ""))}.md`, "utf-8", function(err, data) {
+    if (err) {
+      res.render("error");
+    } else {
+      res.render("page", {
+        title: "test",
+        content: data
+      });
+    }
   });
 });
 
