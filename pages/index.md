e7b380569111f0e25656bb9418675ce44ca3b082
George Fischer <mrpyber@outlook.com>
2018-01-24 12:13:38 +0100

    Lots of changes Make the example page (sort-of) pretty Get started with markdown move to front matter Add SEO

---
+++ pages/index.md
@@ -0,0 +1,13 @@
+---
+title: Example page
+description: A template for content
+tags: example,cmx
+---
+
+![logo](/assets/logo.png)
+# Example page
+This page is a placeholder for what could be great content.
+
+Just edit __index.md__ in the `pages` directory and watch how your content appears!
+
+[![gitlab](https://docs.gitlab.com/assets/images/gitlab-logo.svg) Code on GitLab](https://gitlab.com/zxdev/cmx)
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const config = require("./config/config.def");
 const fs = require('fs');
 const path = require('path');
+const Remarkable = require('remarkable');
+const md = new Remarkable();
+const fm = require('front-matter');
 
 // Config
 Object.assign(config, require("./config/config"));
@@ -14,21 +17,27 @@ app.use("/assets", express.static(path.join(__dirname, "client")));
 
 // Handle requests
 app.use("/", function(req, res) {
-  var pdath = path.join(__dirname, "pages", req.url.replace(/\/$/, "/index.html"));
+  var pdath = path.join(__dirname, "pages", req.url.replace(/\/$/, "/index.md"));
+  var isRender = pdath.match(/\.html$/i) || pdath.match(/\.md$/i);
 
   console.log(pdath);
-  if (pdath.match(/\.html$/i)) {
-    fs.readFile(pdath, "utf-8", function(err, data) {
-      var title = data.match(/<\? (.*?) \?>/i);
-      var titleindex = 1;
+  if (path.extname(pdath) && isRender) {
+    fs.readFile(pdath, "utf-8", function(err, value) {
+      var data = {};
 
       if (err) {
         res.render("error");
       } else {
-
+        data = fm(value);
+        if (path.extname(pdath).toLowerCase() === ".md") {
+          console.log(path.extname(pdath));
+          data.body = md.render(data.body);
+        }
         res.render("page", {
-          title: title && title[titleindex] ? title[titleindex] : "untitled",
-          content: data
+          title: data.attributes.title,
+          description: data.attributes.description,
+          tags: data.attributes.tags,
+          content: data.body.replace(/---/g, "<!--\n$1\n-->")
         });
       }
     });
